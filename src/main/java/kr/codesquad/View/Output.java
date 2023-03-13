3ba0f6c56a60e711d4414acdfaf8be3ff55168cc
sejeong00 <buttermelt@daum.net>
2023-03-13 11:04:36 +0900

    modify 출력 메세지 변경
    이름은 5글자 이하여야 한다는 조건을 추가로 출력하도록 했다.

--- src/main/java/kr/codesquad/View/Output.java
+++ src/main/java/kr/codesquad/View/Output.java
@@ -3,7 +3,7 @@ package kr.codesquad.View;
 public class Output {
 
     public static void printInputPlayerQuestion(){
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        System.out.println("참여할 사람 이름을 입력하세요. (이름은 5글자 이하여하합니다, 이름은 쉼표(,)로 구분하세요)");
     }
     public static void printInputNumberOfPlayerQuestion(){
         System.out.println("참여할 사람은 몇 명인가요?");
